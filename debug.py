d1635390f2be2fb0d94bd916e8d1af45bc7d559b
kevinkrs <kraus.kevin@proton.me>
2023-10-03 10:22:15 +0900

    first experiment run on zero-one-few shot learning and the provided different datasets. Qualitative analysis of output and degree of target achievement

--- debug.py
+++ debug.py
@@ -5,15 +5,15 @@ from utils.eval_utils import prepare_files, calculate_metrics
 raw_eval_output_path = os.path.join(
     "out",
     "eval",
-    f"eval_llama-2-13b-chat-hf-300-v2-4-one_languid-stoat-38_v3_raw.json",
+    f"eval_llama-2-13b-chat-hf-100-v3-4-one_polite-cat-563_v3_raw",
 )
 eval_output_path = os.path.join(
-    "out", "eval", f"eval_llama-2-13b-chat-hf-300-v2-4-one_languid-stoat-38_v3.json"
+    "out", "eval", f"eval_llama-2-13b-chat-hf-100-v3-4-one_polite-cat-563_v3.json"
 )
 trec_eval_output_path = os.path.join(
     "out",
     "eval",
-    f"eval_llama-2-13b-chat-hf-300-v2-4-one_languid-stoat-38_v3_trec.txt",
+    f"eval_llama-2-13b-chat-hf-100-v3-4-one_polite-cat-563_v3_trec.txt",
 )
 
 eval_output_path = prepare_files(raw_eval_output_path, "super_duper_run")
@@ -21,6 +21,6 @@ eval_output_path = prepare_files(raw_eval_output_path, "super_duper_run")
 scores = calculate_metrics(
     eval_output_path=eval_output_path,
     gold_labels_dir="data/gold_labels/",
-    ft_model_name="llama-2-13b-chat-hf-300-v2-4-one",
+    ft_model_name="llama-2-13b-chat-hf-100-v3-4-one",
     run_name="super_duper_run",
 )
