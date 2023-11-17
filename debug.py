9bfad10e773c59f73172043a08dc9d0ba398531c
kevinkrs <kraus.kevin@proton.me>
2023-11-17 14:42:46 +0900

    last mlrun

--- debug.py
+++ debug.py
@@ -10,27 +10,27 @@ from utils.eval_utils import (
 eval_output_path = os.path.join(
     "out",
     "eval",
-    f"eval_d7sm7f5i-293_v7_1200_4_v3.json",
+    f"eval_nd3h2a3n-674_v7_1200_4_v3.json",
 )
 
-eval_df = prepare_files(eval_output_path, "eval_d7sm7f5i-293_v7_1200_4_v3")
+eval_df = prepare_files(eval_output_path, "eval_nd3h2a3n-674_v7_1200_4_v3")
 
 scores = calculate_metrics(
     eval_df=eval_df,
     gold_labels_dir="data/gold_labels/",
     ft_model_name="llama-2-13b-chat-hf-3000-v7-4-v3",
-    run_name="eval_d7sm7f5i-293_v7_1200_4_v3",
+    run_name="eval_nd3h2a3n-674_v7_1200_4_v3",
 )
 
 print(scores)
 
-eval_df = prepare_binary(eval_output_path, "eval_d7sm7f5i-293_v7_1200_4_v3")
+eval_df = prepare_binary(eval_output_path, "eval_nd3h2a3n-674_v7_1200_4_v3")
 
 scores_binary = evaluate_binary(
     eval_df=eval_df,
     gold_labels_dir="data/gold_labels/",
     ft_model_name="llama-2-13b-chat-hf-3000-v7-4-v3",
-    run_name="eval_d7sm7f5i-293_v7_1200_4_v3",
+    run_name="eval_nd3h2a3n-674_v7_1200_4_v3",
 )
 
 print(scores_binary)
